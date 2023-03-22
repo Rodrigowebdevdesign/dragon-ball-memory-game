58aabb31909dcd2c42deaeac30a9d01c9a862aca
rodrigowebdev-design <124701967+rodrigowebdev-design@users.noreply.github.com>
2023-03-21 23:45:19 -0300

    ajustes antes de implementar a regra de negocio para inserir atributo nas cartas

--- js/game.js
+++ js/game.js
@@ -1,19 +1,51 @@
 const grid = document.querySelector('.grid');
 
+const characters = [
+    'babidi',
+    'BojackBido',
+    'cell',
+    'dabura',
+    'freeza',
+    'goku-e-filho',
+    'jiren',
+    'kuririn',
+    'vegeta',
+    'zarbon',
+];
+
 const createElement = (tag, className) =>{
     const element = document.createElement(tag);
     element.className = className;
     return element;
 }
+const revealCard = ({ target }) =>{
+   if(target.parentNode.className.includes('reveal-card')){
+    return;
+   };
+   target.parentNode.classList.add('reveal-card');
+}
 
-const  createCard  = () => {
+const  createCard  = (character) => {
     const card = createElement('div','card');
     const front = createElement('div','face front')
     const back = createElement('div','face back');
 
+    front.style.backgroundImage = `url(../imgs/${character}.jpg)`;
+
     card.appendChild(front);
     card.appendChild(back);
+
+    card.addEventListener('click',revealCard);
     
     return card
 }
-createCard();
+const loadGame = () =>{
+    const duplicateCharacter = [...characters , ...characters];
+    const shufledArray = duplicateCharacter.sort(() => Math.random() -0.5);
+
+    duplicateCharacter.forEach((character) =>{
+        const card = createCard(character);
+        grid.appendChild(card);
+    });
+}
+loadGame()
