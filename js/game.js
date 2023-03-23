5c4518260cefa9b5a7536653a0e58eca5509ea16
rodrigowebdev-design <124701967+rodrigowebdev-design@users.noreply.github.com>
2023-03-23 00:14:18 -0300

    implementação da segunda parte do projeto!!
    aqui foram feitas melhorias no código e também foram implementadas  algumas funcionalidades novas para o projeto!!

--- js/game.js
+++ js/game.js
@@ -1,3 +1,5 @@
+let fistCard = ''
+let secondCard = ''
 const grid = document.querySelector('.grid');
 
 const characters = [
@@ -11,6 +13,8 @@ const characters = [
     'kuririn',
     'vegeta',
     'zarbon',
+    'napa',
+    'broly'
 ];
 
 const createElement = (tag, className) =>{
@@ -18,11 +22,46 @@ const createElement = (tag, className) =>{
     element.className = className;
     return element;
 }
+const checkEndGame = () =>{
+    const disableCards = document.querySelectorAll('.disable-card');
+    if(disableCards.length == 24){
+        // fazer um window location para a pagina de vencedor!!
+    }
+}
+const checkCards = () =>{
+    const firstCharacter = fistCard.getAttribute('data-character');
+    const secondCharacter = secondCard.getAttribute('data-character');
+    if(firstCharacter == secondCharacter){
+        fistCard.firstChild.classList.add('disable-card');
+        secondCard.firstChild.classList.add('disable-card');
+        fistCard = '';
+        secondCard = '';
+
+        checkEndGame()
+    } else{
+        setTimeout(() =>{
+            fistCard.classList.remove('reveal-card');
+            secondCard.classList.remove('reveal-card');
+            fistCard = '';
+            secondCard = '';
+        }, 500)
+      
+    }
+}
 const revealCard = ({ target }) =>{
    if(target.parentNode.className.includes('reveal-card')){
     return;
    };
-   target.parentNode.classList.add('reveal-card');
+   if(fistCard == ''){
+        target.parentNode.classList.add('reveal-card');
+        fistCard = target.parentNode;
+   } else if(secondCard == ''){
+        target.parentNode.classList.add('reveal-card');
+        secondCard = target.parentNode;
+
+        checkCards();
+   }
+   
 }
 
 const  createCard  = (character) => {
@@ -36,6 +75,7 @@ const  createCard  = (character) => {
     card.appendChild(back);
 
     card.addEventListener('click',revealCard);
+    card.setAttribute('data-character',character);
     
     return card
 }
